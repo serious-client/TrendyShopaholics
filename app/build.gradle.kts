b0133b70e15f4a80dcde81413855fc39c5a7b0ca
Nkechi Nnaji <nkechi_nnaji@gallup.com>
2024-11-06 06:20:38 -0600

    Upgraded changes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.nkechinnaji.trendyshopaholics"
         minSdk = 24
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
